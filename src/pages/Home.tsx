0adb060deca00c133688fc11db167d285ccea584
Ahmed-Lasheen1 <ahmed_lasheen1@icloud.com>
2026-09-01 02:12:53 +0300

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -392,7 +392,7 @@ export default function Home({ dark, toggleTheme }: { dark: boolean; toggleTheme
                 transition={{ duration: 0.85, delay: HERO_DELAY }}
                 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
               >
-                <EcgHero height={450} />
+                <EcgHero height={400} />
               </motion.div>
 
               <div>
